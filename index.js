47af3ec17947ad97f04e6f4c25df9015ef4c0e88
Andriy Kyrylenko <46637279+z9877969@users.noreply.github.com>
2023-09-16 02:23:56 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const jsonWrapper = document.querySelector(".output-wrapper");
+const outputJson = jsonWrapper.querySelector(".output");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { value } = e.currentTarget["input-env"];
+  if (!value) return;
+  const envAsObj = value
+    .trim()
+    .split("\n")
+    .map((el) => el.split("=").map((el) => el.trim()))
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+  const envAsJson = JSON.stringify(envAsObj, null, 2);
+  outputJson.textContent = envAsJson;
+});
